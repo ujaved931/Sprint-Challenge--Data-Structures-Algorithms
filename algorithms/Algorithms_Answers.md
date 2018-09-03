bc3e734739e71c9e2bde54f8edc8d170e6216b9e
Usman Javed <ujaved931@gmail.com>
2018-09-04 01:39:54 +0500

    Fixed tests

--- algorithms/Algorithms_Answers.md
+++ algorithms/Algorithms_Answers.md
@@ -1,1 +1,20 @@
 Add your answers to the Algorithms exercises here.
+
+### Ex#1
+a) ologn
+b) ologn   
+c) n^3
+d) n^2
+e) n^4
+f) o1
+g) ologn
+
+### Ex#2
+a) sorted_arr = sorted(arr)
+return sorted_arr[-1] -sorted_arr[0]
+b) We use the quicksort method with pivot being the median. The best way to figure it out. 
+
+### Ex#3
+a) n^2. because of the newer array being called recursively.    
+
+b) nlogn. because of half size of the newer array. 
--- data_structures/ex_1/binary_search_tree.py
+++ data_structures/ex_1/binary_search_tree.py
@@ -5,23 +5,32 @@ class BinarySearchTree:
     self.right = None
 
   def depth_first_for_each(self, cb):
-    cb(self.value)
-    if self.left != None:
-      self.left.depth_first_for_each(cb)
-    if self.right != None:
-      self.right.depth_first_for_each(cb)
+    # cb(self.value)
+    # if self.left != None:
+    #   self.left.depth_first_for_each(cb)
+    # if self.right != None:
+    #   self.right.depth_first_for_each(cb)
+    stack=[]
+    stack.append(self)
+    while len(stack):
+      current_node = stack.pop()
+      if current_node.right:
+        stack.append(current_node.right)
+      if current_node.left:
+        stack.append(current_node.left)
+      cb(current_node.value)
 
   def breadth_first_for_each(self, cb):
     queue = []
     queue.append(self)
 
-    while len(queue) != 0:
-      node = queue.pop(0)
-      cb(node.value)
-      if node.left != None:
-        queue.append(node.left)
-      if node.right != None: 
-        queue.append(node.right)
+    while len(queue):
+      current_node = queue.pop(0)
+      if current_node.left:
+        queue.append(current_node.left)
+      if current_node.right:
+        queue.append(current_node.right)
+      cb(current_node.value)
 
 
   def insert(self, value):
